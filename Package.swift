838854a5ce843c72c5770322b3628618e29e3ce3
Burak Tunc <buraktuncdev@gmail.com>
2022-05-15 09:38:03 +0200

    add details on error message

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/buraktuncdev/WeatherAppLogger.git", .branch("main")),
+        .package(url: "https://github.com/buraktuncdev/WeatherAppLogger.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WeatherAppNetwork",
-            dependencies: [],
+            dependencies: ["WeatherAppLogger"],
             path: "Sources",
             resources: [.process("Resources/Media.xcassets")],
             swiftSettings: [.define("ENABLE_TESTABILITY=YES", .when(configuration: .debug))]),
